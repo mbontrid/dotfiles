9df151445db09297ff4c00a7c6901cb70f38d264
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2025-12-26 13:44:31 +0100

    clean neovim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,6 +4,9 @@
 
 vim.opt.spelllang = { "en", "fr" }
 vim.opt.scrolloff = 10
--- vim.opt.expandtab = false
--- vim.opt.softtabstop = 4
--- vim.opt.tabstop = 4
+
+-- tab conf
+vim.opt.expandtab = false
+vim.opt.tabstop = 4 -- For visual appearance of tab characters
+vim.opt.softtabstop = 4 -- For max jump distance of manual tabs
+vim.opt.shiftwidth = 0 -- Use tabs indentation
