1d7f16319f67403605e77963fedf973b32aa2306
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2025-12-27 23:34:59 +0100

    copilot language server

--- dot_config/nvim/lua/plugins/jupynium.lua
+++ dot_config/nvim/lua/plugins/jupynium.lua
@@ -1,7 +1,9 @@
 return {
   "kiyoon/jupynium.nvim",
   -- build = "pip3 install --user .",
-  build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
+  -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
   -- build = "conda run --no-capture-output -n jupynium pip install .",
-  require = { "rcarriga/nvim-notify", "stevearc/dressing.nvim" },
+  dependencies = { "stevearc/dressing.nvim" },
+  -- "rcarriga/nvim-notify",
+  lazy = true,
 }
