932d937d0ddd633c6a77c055f5b8ae4e266c65a6
Milan Bontridder <119121532+mbontrid@users.noreply.github.com>
2026-01-21 14:17:50 +0100

    update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -10,5 +10,3 @@ vim.opt.expandtab = false
 vim.opt.tabstop = 4 -- For visual appearance of tab characters
 vim.opt.softtabstop = 4 -- For max jump distance of manual tabs
 vim.opt.shiftwidth = 0 -- Use tabs indentation
-
--- vim.lsp.enable(...)
--- dot_config/nvim/lua/plugins/cellular-automaton.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "eandrju/cellular-automaton.nvim",
-  keys = {
-    { "<leader>01", "<cmd>CellularAutomaton make_it_rain<cr>", desc = "CellularAutomaton make it rain" },
-  },
-}
